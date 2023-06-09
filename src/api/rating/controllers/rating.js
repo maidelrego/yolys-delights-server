9ff1cd42d0d8e08466db81583232110458490920
Maydel Rego <maidelrego@yahoo.com>
2023-06-09 14:30:20 -0500

    create ratings is working

--- src/api/rating/controllers/rating.js
+++ src/api/rating/controllers/rating.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * rating controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::rating.rating');
+module.exports = createCoreController("api::rating.rating", ({ strapi }) => ({
+  async create(ctx) {
+    const { data } = ctx.request.body;
+    try {
+      const rating = await strapi
+        .service("api::rating.rating")
+        .create({ data: { ...data } });
+
+      return { rating };
+    } catch (error) {
+      console.log("error", error);
+      ctx.response.status = 500;
+      return { error };
+    }
+  },
+}));
